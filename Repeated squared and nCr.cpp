f439b36883fda57c9e14932598d25f6442eecd4d
Men-cotton <mencotton0410@gmail.com>
2019-06-04 21:38:34 +0900

    add div

--- Repeated squared and nCr.cpp
+++ Repeated squared and nCr.cpp
@@ -1,21 +1,25 @@
 ll mod = 1e9 + 7;
 
-ll Calc_pow(ll x, ll pow) {
-    if (pow == 0) return 1;
-    ll a = Calc_pow(x, pow / 2);
-    a = a * a % mod;
-    if (pow % 2 == 1) a *= x;
-    return a % mod;
-}
-
 vector<ll> fact;
 
-void Init_fact(int n) {
+void init_fact(int n) {
     fact = vector<ll>(n);
     fact[0] = 1;
     for (int i = 1; i < n; i++) fact[i] = fact[i - 1] * i % mod;
 }
 
-ll Combination(ll n, ll r) {
-    return fact[n] * Calc_pow(fact[r] * fact[n - r] % mod, mod - 2) % mod;
+ll calc_pow(ll x, ll pow) {
+    if (pow == 0) return 1;
+    ll a = calc_pow(x, pow / 2);
+    a = a * a % mod;
+    if (pow % 2 == 1) a *= x;
+    return a % mod;
+}
+
+ll calc_div(ll a, ll b) {
+    return a * calc_pow(b, mod - 2) % mod;
+}
+
+ll calc_comb(ll n, ll r) {
+    return calc_div(fact[n], fact[r] * fact[n - r] % mod);
 }
