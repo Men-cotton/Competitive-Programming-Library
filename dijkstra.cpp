e619c6ae3a67c3eef252c284f2089f1dfe686aa4
Mencotton <mencotton0410@gmail.com>
2019-10-24 19:18:56 +0900

    Change How to sort

--- dijkstra.cpp
+++ dijkstra.cpp
@@ -2,30 +2,43 @@
 #include <vector>
 #include <queue>
 
-#define p pair<int,int>
 using namespace std;
 
+struct edge {
+    int to;
+    int cost;
+};
+
+struct status {
+    int cost;
+    int v;
+
+    bool operator<(const status &another) const { return cost < another.cost; };
+
+    bool operator>(const status &another) const { return cost > another.cost; };
+};
+
+
 int n;
-vector<vector<p >> graph;
+vector<vector<edge>> graph;
 vector<int> dis;
 
 void dijkstra(int s) {
-    priority_queue<p, vector<p >, greater<>> que;
+    priority_queue<status, vector<status>, greater<>> que;
     dis = vector<int>(n, 1145141919);
     dis[s] = 0;
-    que.push(p(s, 0));
+    que.push({0, s});
 
     while (!que.empty()) {
-        p now = que.top();
+        status now = que.top();
         que.pop();
-        int nowv = now.first;
 
-        if (dis[nowv] < now.second)continue;
+        if (dis[now.v] < now.cost)continue;
 
-        for (auto next:graph[nowv]) {
-            if (dis[next.first] > dis[nowv] + next.second) {
-                dis[next.first] = dis[nowv] + next.second;
-                que.push(p(next.first, dis[next.first]));
+        for (auto next:graph[now.v]) {
+            if (dis[next.to] > dis[now.v] + next.cost) {
+                dis[next.to] = dis[now.v] + next.cost;
+                que.push({dis[next.to], next.to});
             }
         }
     }
@@ -34,12 +47,12 @@ void dijkstra(int s) {
 int main() {
     int m;
     cin >> n >> m;
-    graph = vector<vector<p >>(n);
+    graph = vector<vector<edge>>(n);
     for (int i = 0; i < m; i++) {
         int from, to, cost;
         cin >> from >> to >> cost;
-        graph[from].push_back(p(to, cost));
-        graph[to].push_back(p(from, cost));
+        graph[from].push_back({to, cost});
+        graph[to].push_back({from, cost});
     }
 
     int start, end;
