f9feee7ce8742fa788bf9c66961c3fd581fe217d
Men-cotton <mencotton0410@gmail.com>
2020-01-11 21:14:56 +0900

    Fix "mod" -> "MOD"

--- Repeated squared and nCr.cpp
+++ Repeated squared and nCr.cpp
@@ -1,26 +1,26 @@
-ll mod = 1e9 + 7;
+ll MOD = 1e9 + 7;
 
 vector<ll> fact;
 
 void init_fact(int n) {
     fact = vector<ll>(n);
     fact[0] = 1;
-    for (int i = 1; i < n; i++) fact[i] = fact[i - 1] * i % mod;
+    for (int i = 1; i < n; i++) fact[i] = fact[i - 1] * i % MOD;
 }
 
 ll calc_pow(ll x, ll pow) {
     if (pow == 0) return 1;
     ll a = calc_pow(x, pow / 2);
-    a = a * a % mod;
+    a = a * a % MOD;
     if (pow % 2 == 1) a *= x;
-    return a % mod;
+    return a % MOD;
 }
 
 ll calc_div(ll a, ll b) {
-    return a * calc_pow(b, mod - 2) % mod;
+    return a * calc_pow(b, MOD - 2) % MOD;
 }
 
 ll calc_comb(ll n, ll r) {
     if (n < r)return 0;
-    return calc_div(fact[n], fact[r] * fact[n - r] % mod);
+    return calc_div(fact[n], fact[r] * fact[n - r] % MOD);
 }
