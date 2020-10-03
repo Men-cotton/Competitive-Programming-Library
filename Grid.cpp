3863fa48c1cc793418f833ea02120b02d8680dcd
Men-cotton <mencotton0410@gmail.com>
2020-10-03 15:25:52 +0900

    Update Grid.cpp

--- Grid.cpp
+++ Grid.cpp
@@ -1,11 +1,23 @@
 struct pos {
     int x, y;
+    pos operator+(pos &rhs) const {
+        return {x + rhs.x, y + rhs.y};
+    }
+    pos &operator+=(const pos &rhs) {
+        x += rhs.x, y += rhs.y;
+        return *this;
+    }
+    bool operator==(pos &rhs) const {
+        return x == rhs.x && y == rhs.y;
+    }
 };
 
 const vector<pos> moves{{1,  0},
                         {-1, 0},
                         {0,  1},
                         {0,  -1}};
+
+int w, h;
 
 bool inRange(pos p) {
     return 0 <= p.x && p.x < h && 0 <= p.y && p.y < w;
